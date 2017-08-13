038d82a7c5e0e5b000efc86b8f8616a321e171b6
blogrido <30958477+blogrido@users.noreply.github.com>
2017-08-13 16:21:32 +0700

    Create Native in-feed.js

---
+++ Native in-feed.js
@@ -0,0 +1,11 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout="image-side"
+     data-ad-layout-key="-fe+6a+2i-k3+sg"
+     data-ad-client="ca-pub-5531330043170221"
+     data-ad-slot="2938733509"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
